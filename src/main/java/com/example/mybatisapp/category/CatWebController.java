c2280ad3587c2309e5e6b236d88d38b64fc63085
boydket <boydket@naver.com>
2024-07-29 16:15:39 +0900

    강사님 첨삭해주신 버전

--- src/main/java/com/example/mybatisapp/category/CatWebController.java
+++ src/main/java/com/example/mybatisapp/category/CatWebController.java
@@ -42,26 +42,87 @@ public class CatWebController {
         return "catweb/category_list";
     }
 
-    @PostMapping("/oldhtml/catweb_old_act")
-    public String categoryOldInsert(@ModelAttribute CategoryDto dto, Model model) {
+    @GetMapping("/category_add")
+    public String categoryAdd() {
+        return "catweb/category_add";
+    }
+
+    @PostMapping("/category_insert")
+    public String categoryInsert(@ModelAttribute CategoryDto dto, Model model) {
         try {
             if (dto == null || dto.getName() == null || dto.getName().isEmpty()) {
                 model.addAttribute("error_message", "이름이 비었습니다.");
                 return "error/error_bad";
             }
             this.categoryService.insert(dto);
-
         } catch (Exception ex) {
             log.error(ex.toString());
             model.addAttribute("error_message", dto.getName()+"중복입니다..");
             return "error/error_save";
         }
-        return "redirect:catweb_old?page=1&name=";  // 브라우저 주소를 redirect 한다.
+        return "redirect:category_list?page=1&name=";  // 브라우저 주소를 redirect 한다.
     }
 
-    @GetMapping("/category_add")
-    public String categoryAdd() {
-        return "catweb/category_add";
+    @GetMapping("/category_view")    // 브라우저의 URL 주소
+    public String categoryView(Model model, @RequestParam Long id) {
+        try {
+            if ( id == null || id <= 0 ) {
+                model.addAttribute("error_message", "ID는 1보다 커야 합니다.");
+                return "error/error_bad";  // resources/templates 폴더안의 화면파일
+            }
+            ICategory find = this.categoryService.findById(id);
+            if ( find == null ) {
+                model.addAttribute("error_message", id + " 데이터가 없습니다.");
+                return "error/error_find";
+            }
+            model.addAttribute("categoryDto", find);
+        } catch (Exception ex) {
+            log.error(ex.toString());
+            model.addAttribute("error_message", "서버 에러입니다. 관리자에게 문의 하세요.");
+            return "error/error_save";  // resources/templates 폴더안의 화면파일
+        }
+        return "catweb/category_view";
     }
 
+    @PostMapping("/category_update")
+    public String categoryUpdate(Model model, @ModelAttribute CategoryDto categoryDto) {
+        try {
+            if (categoryDto == null || categoryDto.getId() <= 0 || categoryDto.getName().isEmpty()) {
+                model.addAttribute("error_message", "id는 1보다 커야하고, name 이 있어야 합니다.");
+                return "error/error_bad";  // resources/templates 폴더안의 화면파일
+            }
+            ICategory find = this.categoryService.findById(categoryDto.getId());
+            if (find == null) {
+                model.addAttribute("error_message", categoryDto.getId() + " 데이터가 없습니다.");
+                return "error/error_find";
+            }
+            this.categoryService.update(categoryDto.getId(), categoryDto);
+        } catch (Exception ex) {
+            log.error(ex.toString());
+            model.addAttribute("error_message", categoryDto.getName() + " 중복입니다.");
+            return "error/error_save";  // resources/templates 폴더안의 화면파일
+        }
+        return "redirect:category_list?page=1&name=";
+    }
+
+    @GetMapping("/category_delete")
+    public String categoryDelete(Model model, @RequestParam Long id) {
+        try {
+            if (id == null || id <= 0) {
+                model.addAttribute("error_message", "id는 1보다 커야 합니다.");
+                return "error/error_bad";  // resources/templates 폴더안의 화면파일
+            }
+            ICategory find = this.categoryService.findById(id);
+            if (find == null) {
+                model.addAttribute("error_message", id + " 데이터가 없습니다.");
+                return "error/error_find";
+            }
+            this.categoryService.delete(id);
+        } catch (Exception ex) {
+            log.error(ex.toString());
+            model.addAttribute("error_message", "서버 에러입니다. 관리자에게 문의하세요.");
+            return "error/error_save";
+        }
+        return "redirect:category_list?page=1&name=";
+    }
 }
