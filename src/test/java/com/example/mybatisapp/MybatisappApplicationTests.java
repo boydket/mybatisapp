05723fe23824aaf64db579d8a4dc60114967ebdc
boydket <boydket@naver.com>
2024-07-29 16:30:40 +0900

    강사님 첨삭 이전으로 되돌림

---
+++ src/test/java/com/example/mybatisapp/MybatisappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatisapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
