45b7530924c18b64ccf8c767caf37e1c137f909d
boydket <boydket@naver.com>
2024-07-30 13:43:01 +0900

    sangbong3에서 붙여넣기

--- src/main/java/com/example/mybatisapp/category/CatWebController.java
+++ src/main/java/com/example/mybatisapp/category/CatWebController.java
@@ -15,50 +15,38 @@ public class CatWebController {
     @Autowired
     private CategoryServiceImpl categoryService;
 
-    @GetMapping("")
-    public String indexHome() {
-        return "indexcat";
-    }
-
-    @GetMapping("/category_list")    // 브라우저의 URL 주소
-    public String categoryOld(Model model, @RequestParam String name, @RequestParam int page) {
+    @GetMapping("/category_list")
+    public String categoryList(Model model, @RequestParam int page, @RequestParam String name) {
         try {
-            if (name == null) {
-                name = "";
-            }
-//            List<ICategory> allList = this.categoryService.getAllList();
             SearchCategoryDto searchCategoryDto = SearchCategoryDto.builder()
-                    .name(name).page(page).build();
+                    .page(page).name(name).build();
             int total = this.categoryService.countAllByNameContains(searchCategoryDto);
+            List<ICategory> list = this.categoryService.findAllByNameContains(searchCategoryDto);
             searchCategoryDto.setTotal(total);
-            List<ICategory> allList = this.categoryService.findAllByNameContains(searchCategoryDto);
-            model.addAttribute("allList", allList);
+            model.addAttribute("categoryList", list);
             model.addAttribute("searchCategoryDto", searchCategoryDto);
         } catch (Exception ex) {
             log.error(ex.toString());
-            model.addAttribute("error_message", "오류가 발생했습니다. 관리자에게 문의하세요.");
-            return "error/error_save";
         }
         return "catweb/category_list";
     }
 
     @GetMapping("/category_add")
-    public String categoryAdd() {
+    public String categoryAdd(Model model) {
+        try {
+
+        } catch (Exception ex) {
+            log.error(ex.toString());
+        }
         return "catweb/category_add";
     }
 
     @PostMapping("/category_insert")
-    public String categoryInsert(@ModelAttribute CategoryDto dto, Model model) {
+    public String categoryInsert(@ModelAttribute CategoryDto dto) {
         try {
-            if (dto == null || dto.getName() == null || dto.getName().isEmpty()) {
-                model.addAttribute("error_message", "이름이 비었습니다.");
-                return "error/error_bad";
-            }
             this.categoryService.insert(dto);
         } catch (Exception ex) {
             log.error(ex.toString());
-            model.addAttribute("error_message", dto.getName()+"중복입니다..");
-            return "error/error_save";
         }
         return "redirect:category_list?page=1&name=";  // 브라우저 주소를 redirect 한다.
     }
@@ -66,62 +54,30 @@ public class CatWebController {
     @GetMapping("/category_view")    // 브라우저의 URL 주소
     public String categoryView(Model model, @RequestParam Long id) {
         try {
-            if ( id == null || id <= 0 ) {
-                model.addAttribute("error_message", "ID는 1보다 커야 합니다.");
-                return "error/error_bad";  // resources/templates 폴더안의 화면파일
-            }
-            ICategory find = this.categoryService.findById(id);
-            if ( find == null ) {
-                model.addAttribute("error_message", id + " 데이터가 없습니다.");
-                return "error/error_find";
-            }
-            model.addAttribute("categoryDto", find);
+            ICategory categoryDto = this.categoryService.findById(id);
+            model.addAttribute("categoryDto", categoryDto);
         } catch (Exception ex) {
             log.error(ex.toString());
-            model.addAttribute("error_message", "서버 에러입니다. 관리자에게 문의 하세요.");
-            return "error/error_save";  // resources/templates 폴더안의 화면파일
         }
         return "catweb/category_view";
     }
 
     @PostMapping("/category_update")
-    public String categoryUpdate(Model model, @ModelAttribute CategoryDto categoryDto) {
+    public String categoryUpdate(@ModelAttribute CategoryDto dto) {
         try {
-            if (categoryDto == null || categoryDto.getId() <= 0 || categoryDto.getName().isEmpty()) {
-                model.addAttribute("error_message", "id는 1보다 커야하고, name 이 있어야 합니다.");
-                return "error/error_bad";  // resources/templates 폴더안의 화면파일
-            }
-            ICategory find = this.categoryService.findById(categoryDto.getId());
-            if (find == null) {
-                model.addAttribute("error_message", categoryDto.getId() + " 데이터가 없습니다.");
-                return "error/error_find";
-            }
-            this.categoryService.update(categoryDto.getId(), categoryDto);
+            this.categoryService.update(dto.getId(), dto);
         } catch (Exception ex) {
             log.error(ex.toString());
-            model.addAttribute("error_message", categoryDto.getName() + " 중복입니다.");
-            return "error/error_save";  // resources/templates 폴더안의 화면파일
         }
         return "redirect:category_list?page=1&name=";
     }
 
     @GetMapping("/category_delete")
-    public String categoryDelete(Model model, @RequestParam Long id) {
+    public String categoryDelete(@RequestParam Long id) {
         try {
-            if (id == null || id <= 0) {
-                model.addAttribute("error_message", "id는 1보다 커야 합니다.");
-                return "error/error_bad";  // resources/templates 폴더안의 화면파일
-            }
-            ICategory find = this.categoryService.findById(id);
-            if (find == null) {
-                model.addAttribute("error_message", id + " 데이터가 없습니다.");
-                return "error/error_find";
-            }
             this.categoryService.delete(id);
         } catch (Exception ex) {
             log.error(ex.toString());
-            model.addAttribute("error_message", "서버 에러입니다. 관리자에게 문의하세요.");
-            return "error/error_save";
         }
         return "redirect:category_list?page=1&name=";
     }
